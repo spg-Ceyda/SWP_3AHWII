34845cb628df8928e2f00a950229ee3f18b3e1e5
spg-Ceyda <ARS210315@spengergasse.at>
2024-04-08 23:47:59 +0200

    hü

---
+++ 2024-04-02_plf_verb/query.js
@@ -0,0 +1,40 @@
+console.log('here is my query:');
+// TODO
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function getwatchlistsname(benutzerId) {
+    const benutzer = await prisma.benutzer.findUnique({
+        where: { id: benutzerId},
+        include: { Watchlist: true},
+    });
+    return benutzer.watchlists
+}
+
+
+async function getwatchtracks(watchlists) {
+    const watchlist = await prisma.watchlists.findUnique({
+        where: { id: watchlists},
+        include: { tracks: true},
+    });
+    return watchlist.tracks;
+}
+
+
+async function main() {
+    try {
+      const watchlistnames = await getwatchlistnames(1);
+      console.log('Watchlist-Namen für Benutzer mit ID 1:', getwatchlistsname);
+  
+      const trackswatchlist = await getwatchtracks(1);
+      console.log('Tracks in Watchlist mit ID 1:', getwatchtracks);
+    } 
+    catch (error) {
+      console.error('Error:', error);
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+  
+  main();
