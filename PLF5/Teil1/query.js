82cd715750116e052479631441dd0e21d9598f2b
spg-Ceyda <ARS210315@spengergasse.at>
2024-05-27 22:44:45 +0200

    hü test verbessern plf5

--- PLF5/Teil1/query.js
+++ PLF5/Teil1/query.js
@@ -1,2 +1,74 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+
+ 
+async function findZooByName(name){
+    try{
+        const zoos = await prisma.zoo.findMany({
+            select: { id: true, land: true, stadt: true, adresse: true, baujahr: true },
+        });
+        if (!zoos) {
+            throw new Error(`Zoo mit dem Namen: ${name} wurde nicht gefunden.`);
+        }
+        console.log('Gefundener Zoo:', zoos);
+
+
+        const zooId = zoos[0].id; 
+        const zoo = await prisma.zoo.findUnique({
+            where: { id: zooId },
+            include: { abteilungen: true },
+        });
+        console.log(`Infos über den Zoo mit der ID: ${zooId}:`, zoo);
+        
+
+        const abteilungen = await prisma.abteilung.findMany({
+            where: { zooId },
+        });
+          console.log(`Alle Abteilungen des Zoos mit der ID: ${zooId}:`,zoo);
+        
+
+
+        for (const abteilung of abteilungen) {
+            const tiereCount = await prisma.tier.count({
+              where: { abteilungId: abteilung.id },
+            });
+            console.log(`Abteilung ${abteilung.name} hat ${tiereCount} Tiere`);
+          
+            const mitarbeiter = await prisma.mitarbeiter.findMany({
+                where: {
+                  abteilungen: {
+                    some: { zooId },
+                  },
+                },
+                include: { abteilungen: true },
+              });
+              console.log(`Alle Mitarbeiter des Zoos mit der ID ${zooId}:`, mitarbeiter);
+            
+              
+            const mitarbeiterId = mitarbeiter[0].id; 
+              const mitarbeiterDetails = await prisma.mitarbeiter.findUnique({
+                where: { id: mitarbeiterId },
+                include: { abteilungen: { include: { tiere: true } } },
+              });
+            
+              let tiereBetreut = 0;
+              const tierNamen = [];
+            
+            }}
+
+catch (error) {
+    console.error('Fehler beim Abrufen der Daten:', error);
+} finally {
+    await prisma.$disconnect();
+}
+}
+
+
+async function main() {
+    await findZooByName('BeispielZooName');
+}
+
+    
+
+main();
--- PLF5/Teil1/seed.js
+++ PLF5/Teil1/seed.js
@@ -1,29 +1,54 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const { faker } = require('@faker-js/faker');
+const { fakerDE } = require('@faker-js/faker');
 
 
 async function main() {
     for (let i = 0; i < 5; i++) {
-        const Zoo = await prisma.Zoo.create();
-        data: {
-            land; faker.adresse.country(),
-                stadt; faker.adresse.city(),
-                    adresse; faker.adresse.streetAdresse(),
-                        baujahr; faker.date.past().getFullYear();
-        }
-    }
-};
+        const zoo = await prisma.zoo.create({
+            data: {
+            land: fakerDE.location.country(),
+            stadt: fakerDE.location.city(),
+            adresse: fakerDE.location.streetAddress(),
+            baujahr: fakerDE.date.past().getFullYear(),
+        },
+    });
 
+    let abteilung;
+    for (let j = 0; j < fakerDE.number.int({min: 2, max: 7}); j++) {
+         abteilung = await prisma.abteilung.create({
+            data: {
+            name: fakerDE.animal.type(),
+            zooId: zoo.id,
+            },
+        });
+    }       
+        
+    for (let c = 0; c < fakerDE.number.int({min: 5, max: 20}); c++) {
+        const tier =  await prisma.tier.create({
+            data: {
+            name: fakerDE.person.firstName(),
+            art: fakerDE.animal.type(),
+            abteilungId: abteilung.id,
+            
+            },
+        });
+    }     
 
+    for (let h = 0; h < 100; h++) {
+        await prisma.mitarbeiter.create({
+            data: {
+            name: fakerDE.person.firstName(),
+            
+        },
+    });
+    
+}
+}
+}
 
 
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+    .then ((rw) => console.log('seeding done: ', rw))
+    .catch((e) => console.log('Es gab Fehler', e.message));
+
